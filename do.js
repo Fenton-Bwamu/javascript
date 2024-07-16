70a8b40a0cb2925e006a4bc569daacbc25a53452
fentoncollins123456789 <fentoncollins29@gmail.com>
2024-07-16 08:46:48 +0300

    initial commit

--- do.js
+++ do.js
@@ -1,5 +1,16 @@
-let i =0;
-do{
-    console.log("do...while loop iteration:" + i);
-    i = i + 1;
-} while (i < 5);
+// let i =0;
+// do{
+//     console.log("do...while loop iteration:" + i);
+//     i = i + 1;
+// } while (i < 5);
+
+
+let number = 0; // Initialize the starting point
+
+// Simulate a do-while loop
+do {
+    if (number % 2 === 0) { // Check if the number is even
+        console.log(number); // Print the number if it is even
+    }
+    number++; // Increment the number
+} while (number <= 20); // Loop until the number is less than or equal to 20
--- while.js
+++ while.js
@@ -1,5 +1,18 @@
-let i = 0;
-while (i < 6){
-    console.log("while loop iteretion:" +i);
-    i=i+1;
+// let i = 0;
+// while (i < 6){
+//     console.log("while loop iteretion:" +i);
+//     i=i+1;
+// }
+
+
+
+
+let number = 0; // Initialize the starting point
+
+// Loop until the number is less than or equal to 20
+while (number <= 20) {
+    if (number % 2 === 0) { // Check if the number is even
+        console.log(number); // Print the number if it is even
+    }
+    number++; // Increment the number
 }
